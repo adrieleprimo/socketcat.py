dcb86adbbfbc7bfc90e42f08b6e9a632e5f9b6d5
Adriele <adriele.dejprimo@gmail.com>
2024-10-18 19:50:41 -0300

    More functions related to dealing with clients

--- src/clientHandler.py
+++ src/clientHandler.py
@@ -9,3 +9,35 @@ def runCommand(command):
         output = 'Failed to execute command.\r\n'
 
     return output
+
+def clientHandler(clientSocket, uploadDestination=None, execute=None, command=False):
+    if uploadDestination:
+        fileBuffer = b''
+
+        while True:
+            data = clientSocket.recv(1024)
+            if not data:
+                break
+            else:
+                fileBuffer+=data
+
+        try:
+            with open(uploadDestination, 'wb') as f:
+                f.write(fileBuffer)
+            clientSocket.send(f'Successfully saved file to {uploadDestination}\r\n'.encode())
+        except:
+            clientSocket.send(f'Failed to save file to {uploadDestination}\r\n'.encode())
+        
+    if execute:
+        output = runCommand(execute)
+        clientSocket.send(output.encode())
+
+    if command:
+        while True:
+            clientSocket.send(b'socketCat:#> ')
+            cmdBuffer = b''
+            while b'\n' not in cmdBuffer:
+                cmdBuffer +=clientSocket.recv(1024)
+
+    response = runCommand(cmdBuffer.decode())   
+    clientSocket.send(response.encode())
