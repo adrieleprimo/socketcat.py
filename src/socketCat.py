f540fcba3845380e7df2c85ab3b1c99bc57ef45f
Adriele <adriele.dejprimo@gmail.com>
2024-10-22 17:59:15 -0300

    A SSL Layer to my clientSender

--- src/socketCat.py
+++ src/socketCat.py
@@ -30,6 +30,7 @@ def usage():
 
 def clientSender(buffer):
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client = ssl.wrap_socket(client, ssl_version=ssl.PROTOCOL_TLSv1_2)
 
     try:
         client.connect((target,port))
