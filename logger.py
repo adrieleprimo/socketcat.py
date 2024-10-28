04c271bda447610153ed339944c14f5fa243d736
Adriele <adriele.dejprimo@gmail.com>
2024-10-28 17:16:52 -0300

    Connection monitoring function

--- logger.py
+++ logger.py
@@ -1,8 +1,16 @@
 import logging
-import datetime
+from datetime import datetime
 
 logging.basicConfig(
     filename=f'socketCat_{datetime.now().strftime('%Y%m%d')}.log',
     level=logging.DEBUG,
     format='%(asctime)s - %(levelname)s - %(message)s'
 )
+
+def logConnection(clientAddress, port, direction='inbound'):
+    if direction == 'inbound':
+        logging.info(f'Connection received from {clientAddress}:{port}')
+    else:
+        logging.info(f'Connection established to {clientAddress}:{port}')
+
+    
