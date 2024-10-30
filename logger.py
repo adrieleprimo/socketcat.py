0cb52f3193cc7176bf46b1eae4cc1bf935eafeb6
Adriele <adriele.dejprimo@gmail.com>
2024-10-30 19:07:13 -0300

    Fixing quotation marks in the format

--- logger.py
+++ logger.py
@@ -2,7 +2,7 @@ import logging
 from datetime import datetime
 
 logging.basicConfig(
-    filename=f'socketCat_{datetime.now().strftime('%Y%m%d')}.log',
+    filename=f'socketCat_{datetime.now().strftime("%Y%m%d")}.log',
     level=logging.DEBUG,
     format='%(asctime)s - %(levelname)s - %(message)s'
 )
