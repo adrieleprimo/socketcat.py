10da7ed7a62e7faab32051be633df46843897a59
Adriele <adriele.dejprimo@gmail.com>
2024-11-05 17:47:04 -0300

    The same size

--- aesCipher.py
+++ aesCipher.py
@@ -2,3 +2,10 @@ from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
 from Crypto.Random import get_random_bytes
 import base64
+
+keySize = 32
+
+class aesCipher:
+    def __init__(self, key):
+        self.key = key[:keySize].ljust(keySize, b'\0')
+    
